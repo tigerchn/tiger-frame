1bce64a87510527cbb8796f43e0770947faf39a1
tigerchn <lxm.js.chn@gmail.com>
2026-05-08 11:44:06 +0800

    问题修复

--- tiger-common/tiger-common-log/src/main/java/com/frame/log/aspect/LogAspect.java
+++ tiger-common/tiger-common-log/src/main/java/com/frame/log/aspect/LogAspect.java
@@ -1,6 +1,6 @@
 package com.frame.log.aspect;
 
-import com.google.gson.Gson;
+import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -16,23 +16,29 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(name = {"log.aspect.enable"}, havingValue = "true", matchIfMissing = true)
 public class LogAspect {
 
-    @Pointcut("execution(* com.frame.*.controller.*Controller.*(..)) || execution(* com.frame.*.service.*Service.*(..))")
+    @Pointcut("execution(* com.frame..*.controller.*Controller.*(..)) || execution(* com.frame..*.service.*Service.*(..))")
     private void pointCut() {
     }
 
     @Around("pointCut()")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         Object[] reqArgs = pjp.getArgs();
-        String req = new Gson().toJson(reqArgs);
+        String req = JSON.toJSONString(reqArgs);
         MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
         String methodName = methodSignature.getDeclaringType().getName() + "." + methodSignature.getName();
         log.info("{},req:{}", methodName, req);
-        Long startTime = System.currentTimeMillis();
-        Object responseObj = pjp.proceed();
-        String resp = new Gson().toJson(responseObj);
-        Long endTime = System.currentTimeMillis();
-        log.info("{},response:{},costTime:{}", methodName, resp, endTime - startTime);
-        return responseObj;
+        long startTime = System.currentTimeMillis();
+        try {
+            Object responseObj = pjp.proceed();
+            String resp = JSON.toJSONString(responseObj);
+            long endTime = System.currentTimeMillis();
+            log.info("{},response:{},costTime:{}", methodName, resp, endTime - startTime);
+            return responseObj;
+        } catch (Throwable e) {
+            long endTime = System.currentTimeMillis();
+            log.error("{},exception:{},costTime:{}", methodName, e.getMessage(), endTime - startTime, e);
+            throw e;
+        }
     }
 
 }
