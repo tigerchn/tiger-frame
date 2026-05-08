f64886148999b34e0505c870cda3f0622a5ad6c3
tigerchn <lxm.js.chn@gmail.com>
2026-05-08 16:47:17 +0800

    日志记录位置调整

---
+++ tiger-demo/src/test/java/com/frame/demo/DemoTests.java
@@ -0,0 +1,13 @@
+package com.frame.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("***********************************************");
+    }
+}
